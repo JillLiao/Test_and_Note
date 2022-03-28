f4b3b6db986a2782fadc46b0a6917d99aa487e9e
User <User@Jill-PC>
2022-03-28 21:40:58 +0800

    java

--- src/main/java/test/_01_StringTokenizerDemo.java
+++ src/main/java/test/_01_StringTokenizerDemo.java
@@ -34,8 +34,6 @@ public class _01_StringTokenizerDemo {
 			s3 = s3.concat(tt.nextToken("!"));
 			s3 = s3.concat(tt.nextToken("!"));
 		System.out.println(s3);
-		
-		//tt
 	}
 	/*
 	 * ※※StringTokenizer 類別	→ 用以去除字串中指定的字符
